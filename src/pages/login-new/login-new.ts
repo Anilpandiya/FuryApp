244f6b74bb3f59b433fb3fd80eef6ae9500303ba
Anil Kumar Pandiya <anilpandiya05@gmail.com>
2018-08-29 18:42:28 +0530

    Update login-new.ts

--- src/pages/login-new/login-new.ts
+++ src/pages/login-new/login-new.ts
@@ -2,12 +2,9 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { OtpverifyPage } from '../otpverify/otpverify';
 
-/**
- * Generated class for the LoginNewPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import * as $ from 'jquery';
+
+
 
 @Component({
   selector: 'page-login-new',
@@ -15,19 +12,45 @@ import { OtpverifyPage } from '../otpverify/otpverify';
 })
 export class LoginNewPage {
 
-  phoneNumber : any;
+
+  otp: Number;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.otp = Math.floor(Math.random() * Math.floor(9999));
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginNewPage');
+    console.log('ionViewDidLoad LoginNewPage', this.otp);
   }
 
-  signIn(phoneNumber){
-    
-    this.navCtrl.push(OtpverifyPage,{
-      data : phoneNumber
+  signIn(phoneNumber: Number) {
+
+    let otp = this.otp;
+
+     var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "http://api.msg91.com/api/v2/sendsms",
+      "method": "POST",
+      "headers": {
+        "authkey": "120364AWey9QFL5b8423c6",
+        "content-type": "application/json"
+      },
+      "processData": false,
+      "data": "{ \"sender\": \"SOCKET\", \"route\": \"4\", \"country\": \"91\", \"sms\": [ { \"message\": \"Fury App Otp : 6969\", \"to\": [ \"9672598162\"] } ] }"
+    }
+
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      alert("Sms Sent!");
+    });
+
+    this.navCtrl.push(OtpverifyPage, {
+      phoneNumber: phoneNumber,
+      otp: otp
     });
     return 0;
   }
+
+
 }
