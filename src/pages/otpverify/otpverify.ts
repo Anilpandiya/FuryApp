35865ba441bc2771d430a8e59c7094e887deaa21
Anil Kumar Pandiya <anilpandiya05@gmail.com>
2018-08-29 18:43:22 +0530

    Update otpverify.ts

--- src/pages/otpverify/otpverify.ts
+++ src/pages/otpverify/otpverify.ts
@@ -1,30 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
+import { AlertController } from 'ionic-angular';
 
 @Component({
-    selector: 'page-otpverify',
-    templateUrl: 'otpverify.html',
-  })
+  selector: 'page-otpverify',
+  templateUrl: 'otpverify.html',
+})
 
-  export class OtpverifyPage {
+export class OtpverifyPage {
 
-    phoneNumber : number;
-    otp : any;
-  
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
-      this.phoneNumber = navParams.get('data');
-    }
-  
-   
+  phoneNumber: Number;
+  otp: Number;
+  user_otp: Number;
 
-    
-    ionViewDidLoad() {
-      console.log('ionViewDidLoad LoginPage');
-    }
-  
-    verifyOtp(otp){
-      this.navCtrl.push(TabsPage);
-      return 0;
-    }
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
+    this.phoneNumber = navParams.get('phoneNumber');
+    this.otp = navParams.get('otp');
   }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad otpverifyPage PhNo: ', this.phoneNumber, 'Otp :', this.otp);
+  }
+
+  presentAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'InValid OTP !',
+      subTitle: 'Please Enter correct one.',
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
+
+  verifyOtp(user_otp) {
+    if (user_otp == this.otp) { this.navCtrl.push(TabsPage); }
+    else {
+      this.presentAlert();
+    }
+    return 0;
+  }
+}
